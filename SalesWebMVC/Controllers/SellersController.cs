051417a93c2247692367ad6c9de7346220d2d83c
glaucoserrano <glaucoserrano@gmail.com>
2023-08-24 11:13:50 -0300

    Validation

--- SalesWebMVC/Controllers/SellersController.cs
+++ SalesWebMVC/Controllers/SellersController.cs
@@ -37,6 +37,14 @@ namespace SalesWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Create(Seller seller)
         {
+            if (!ModelState.IsValid)
+            {
+                List<Department> departments = _departmentService.FindAll();
+                SellerFormViewModel viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+
+                return View(viewModel);
+            }
+
             _sellerService.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
@@ -102,7 +110,15 @@ namespace SalesWebMVC.Controllers
         [ValidateAntiForgeryToken]
         public IActionResult Edit(int id, Seller seller)
         {
-            if(id != seller.Id)
+            if (!ModelState.IsValid)
+            {
+                List<Department> departments = _departmentService.FindAll();
+                SellerFormViewModel viewModel = new SellerFormViewModel { Seller = seller, Departments = departments };
+
+                return View(viewModel);
+            }
+
+            if (id != seller.Id)
             {
                 return RedirectToAction(nameof(Error), new { message = "Id mismatch" });
             }
--- SalesWebMVC/Models/Seller.cs
+++ SalesWebMVC/Models/Seller.cs
@@ -9,18 +9,26 @@ namespace SalesWebMVC.Models
     {
         public int Id { get; set; }
         [Display(Name="Nome")]
+        [Required(ErrorMessage ="{0} é obrigatório")]
+        [StringLength(60,MinimumLength =3,ErrorMessage ="Tamanho do {0} deve ser entre {2} e {1} caracteres")]
         public string Name { get; set; }
         [Display(Name = "Email")]
+        [EmailAddress(ErrorMessage ="Email é obrigatório")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
         [DataType(DataType.EmailAddress)]
         public string Email { get; set; }
         [Display(Name = "Aniversário")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
         [DataType(DataType.Date)]
         [DisplayFormat(DataFormatString ="{0:dd/MM/yyyy}")]
         public DateTime BirthDate { get; set; }
         [Display(Name= "Salário")]
         [DisplayFormat(DataFormatString ="{0:F2}")]
+        [Required(ErrorMessage = "{0} é obrigatório")]
+        [Range(100.0,5000.0,ErrorMessage ="{0} deve ser entre {1} e {2}")]
         public double BaseSalary { get; set; }
         public Department Department { get; set; }
+        [Display(Name ="Departamento")]
         public int DepartmentId { get; set; }
 
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
